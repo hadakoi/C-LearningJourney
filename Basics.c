a0174ce8abdd22ca0ed52c3376746ac85de948b2
hadakoi <aryguy2005@gmail.com>
2024-06-27 17:27:33 +0530

    first commit with basics of code

---
+++ Basics.c
@@ -0,0 +1,21 @@
+#include <stdio.h> // This is the file header and should be included on all c files.
+
+// This is a comment using the representation of "//" and we can also use 
+
+/*
+To Create Multiple Line Comments
+As can be Seen
+*/
+
+int main(){ //this represents the main body of code to be executed
+
+  int x; // basic declaration of making the x variable an integer
+
+  x = 5; // Adds a value to x
+
+  printf("%d", x); // Prints the variable x
+
+  return 0; // shows its the end of the code
+}
+
+//Important note to add ; after every Line of Code
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # C-LearningJourney
 This is the Journey
+<br>
+Author - Aryan Kudva
+This is the start for me hopefully a change
