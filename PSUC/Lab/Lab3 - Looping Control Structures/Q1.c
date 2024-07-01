58c08cc001c9190cf029edba98e46d25187d7fd6
Aryan Kudva <148073897+hadakoi@users.noreply.github.com>
2024-07-01 18:08:07 +0530

    Q1 Lab3

---
+++ PSUC/Lab/Lab3 - Looping Control Structures/Q1.c
@@ -0,0 +1,34 @@
+/*
+do the following with the help of iterative (looping) control structures such as while and do-while statements
+Reverse a given number and check if it is a palindrome or not.
+*/
+
+#include <stdio.h>
+
+int main(){
+
+   int Number, Reversed, DynamicNumber, Temp;
+
+   printf("Enter Number you Wish to Reverse: ");
+   scanf("%d", &Number);
+
+   DynamicNumber = Number;
+
+   while(DynamicNumber != 0){
+      Temp = DynamicNumber % 10;
+      DynamicNumber = DynamicNumber / 10;
+      Reversed = Reversed*10 + Temp;
+   }
+
+   printf("The Reversed Number is %d \n", Reversed);
+
+   if(Reversed = Number){
+    printf("%d is a palindrome", Number);
+   }
+   else{
+      printf("Not a Palindrome");
+   }
+
+   return 0;
+
+}
