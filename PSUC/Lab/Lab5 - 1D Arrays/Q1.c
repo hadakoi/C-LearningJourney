cd95d7bef1d4eda84549f926addfbafbd684da71
hadakoi <aryguy2005@gmail.com>
2024-07-03 10:34:12 +0530

    Lab5 Q1

---
+++ PSUC/Lab/Lab5 - 1D Arrays/Q1.c
@@ -0,0 +1,40 @@
+/*
+With the knowledge of 1D arrays
+Find the largest and smallest element in a 1D array.
+*/
+
+#include <stdio.h>
+
+int main(){
+   int n, x;
+   printf("Enter the number of elements in the array: ");
+   scanf("%d", &n);
+
+   int arr[n];
+   printf("Enter the elements of the array:\n");
+
+   for(x=0; x<n ;x++){
+      printf("Enter Element %d: ", x+1);
+      scanf("%d", &arr[x]);
+   }
+
+   int largest, smallest;
+
+   largest = arr[0];
+   smallest = arr[0];
+
+   for(x=1; x<n ;x++){
+      if (arr[x] > largest) {
+            largest = arr[x];
+        }
+        if (arr[x] < smallest) {
+            smallest = arr[x];
+        }
+   }
+
+   printf("Largest element: %d\n", largest);
+   printf("Smallest element: %d\n", smallest);
+
+   return 0;
+
+}
