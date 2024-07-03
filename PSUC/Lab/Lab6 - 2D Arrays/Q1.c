5912c8319dfbf16101a0af8d3e47f146d79f2e9e
hadakoi <aryguy2005@gmail.com>
2024-07-03 23:48:31 +0530

    Q1 of Lab6

--- PSUC/Lab/Lab6 - 2D Arrays/Q1.c
+++ PSUC/Lab/Lab6 - 2D Arrays/Q1.c
@@ -1,1 +1,43 @@
+/*
+With the knowledge of 2D arrays
+Find whether a given matrix is symmetric or not.
+*/
 
+#include <stdio.h>
+
+int main() {
+
+   int n;
+   printf("Enter the size of the matrix (n x n): ");
+   scanf("%d", &n);
+
+   int matrix[n][n];
+    
+   // Input the matrix
+   printf("Enter the elements of the matrix:\n");
+   for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+         scanf("%d", &matrix[i][j]);
+      }
+   }
+
+    // Check for symmetry
+    int symmetric = 1; // Assume the matrix is symmetric initially
+    for (int i = 0; i < n && symmetric; i++) {
+        for (int j = 0; j < n; j++) {
+            if (matrix[i][j] != matrix[j][i]) {
+                symmetric = 0; // Not symmetric
+                break;
+            }
+        }
+    }
+
+    if (symmetric == 1) {
+        printf("The matrix is symmetric.\n");
+    } else {
+        printf("The matrix is not symmetric.\n");
+    }
+
+    return 0;
+   
+}
