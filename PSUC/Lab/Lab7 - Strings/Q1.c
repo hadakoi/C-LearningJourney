42cfb1d2d7195ebe3e996db3cafc626191c97850
Aryan Kudva <148073897+hadakoi@users.noreply.github.com>
2024-07-06 18:40:25 +0530

    Update Q1.c of lab7

--- PSUC/Lab/Lab7 - Strings/Q1.c
+++ PSUC/Lab/Lab7 - Strings/Q1.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+int main() {
+   
+   char sentence[1000];
+   int wordCount = 0;
+   int inWord = 0;
+
+   printf("Enter a sentence: ");
+   fgets(sentence, sizeof(sentence), stdin);
+
+   for (int i = 0; sentence[i] != '\0'; i++) {
+      if (sentence[i] == ' ' || sentence[i] == '\n' || sentence[i] == '\t') {
+         inWord = 0;
+      } else if (inWord == 0) {
+         inWord = 1;
+         wordCount++;
+      }
+   }
+
+   printf("Number of words: %d\n", wordCount);
+
+   return 0;
+
+}
 
