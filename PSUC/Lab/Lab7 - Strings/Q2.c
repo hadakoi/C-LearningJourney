e80f8c8dbcda423b5ed06e993c92d6621b2904b6
hadakoi <aryguy2005@gmail.com>
2024-07-08 13:04:44 +0530

    Lab7 q2

---
+++ PSUC/Lab/Lab7 - Strings/Q2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+   
+    char str[100];
+    int i = 0;
+
+   printf("Enter a string: ");
+   gets(str);  
+
+   // Toggle the case of each character
+   while (str[i] != '\0') {
+      if (str[i] >= 'a' && str[i] <= 'z') {
+         str[i] = str[i] - 'a' + 'A'; // Convert lowercase to uppercase
+      } else if (str[i] >= 'A' && str[i] <= 'Z') {
+         str[i] = str[i] - 'A' + 'a'; // Convert uppercase to lowercase
+      }
+      i++;
+   }
+
+    printf("Toggled string: %s", str);
+
+    return 0;
+
+}
