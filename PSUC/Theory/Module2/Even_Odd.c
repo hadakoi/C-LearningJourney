f305ad0cba766c5792997117b674519e4c957287
hadakoi <aryguy2005@gmail.com>
2024-07-26 23:28:43 +0530

    1st code done

--- PSUC/Theory/Module2/Even_Odd.c
+++ PSUC/Theory/Module2/Even_Odd.c
@@ -3,16 +3,16 @@
 #include <stdio.h>
 
 int main(){
+
 int x;
-printf("input an integer\n");
+printf("input an integer : ");
 scanf("%d" ,&x);
 
-if ((x % 2) == 0) {
+if ((x % 2) == 0) { // if there is no remainder its even
 printf("It is an even number\n");
-}
+} 
 
-if ((x % 2) == 1)
-{
+if ((x % 2) == 1) { // if there is a remainder its odd
 printf("It is an odd number\n");
 }
 
