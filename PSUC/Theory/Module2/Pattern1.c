608f6feb11be24a8fe57c2637711f83570c1d9f2
hadakoi <aryguy2005@gmail.com>
2024-07-30 22:02:57 +0530

    work

--- PSUC/Theory/Module2/Pattern1.c
+++ PSUC/Theory/Module2/Pattern1.c
@@ -1,12 +1,11 @@
 /*
 Using For Loops print the pattern ->
 
-Taking i as line and j as amount of *
-*       Logic -> i = 1, j = 1
-**               i = 2, j = 2 
-***              i = 3, j = 3
-****             i = 4, j = 4
-*****            i = 5, j = 5
+*       
+**               
+***            
+****             
+*****            
 
 and so on depending on number of lines user enters.
 */
@@ -14,6 +13,7 @@ and so on depending on number of lines user enters.
 #include <stdio.h>
 
 int main(){
+   
    int i, j, n;
 
    printf("Enter Number of lines to print the *: ");
