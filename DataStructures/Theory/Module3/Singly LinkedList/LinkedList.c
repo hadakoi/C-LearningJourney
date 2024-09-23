81a84534d6766e374520406d5d06744f3b78f91f
hadakoi <aryguy2005@gmail.com>
2024-09-24 00:06:11 +0530

    Linked List Basic

--- DataStructures/Theory/Module3/Singly LinkedList/LinkedList.c
+++ DataStructures/Theory/Module3/Singly LinkedList/LinkedList.c
@@ -1,46 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define the NODE structure using typedef
 typedef struct node {
     int info;
     struct node *link;
-} NODE;
+} *NODE; // NODE is now a pointer to struct node
 
-NODE* NewNode(int item){
-  NODE *TempNode = (NODE *)malloc(sizeof(NODE));
-
-  TempNode->info = item;
-  TempNode->link = NULL;
-
-  return TempNode;
-
+NODE NewNode(int item) {
+    NODE TempNode = (NODE)malloc(sizeof(struct node)); // Allocate memory for a struct node
+    TempNode->info = item; // Assign value to info
+    TempNode->link = NULL; // Initialize link to NULL
+    return TempNode; // Return pointer to the new node
 }
 
-void DisplayNode(NODE *head){
-
-  NODE *current = head;
-
-  while(current != NULL){
-    printf("%d", current->info);
-    current = current->link;
-    printf("\n");
-  }
+void DisplayNode(NODE head) {
+    NODE current = head;
+    while (current != NULL) {
+        printf("%d\n", current->info);
+        current = current->link;
+    }
 }
 
-int main(){
-  NODE *head;
-  NODE *second;
-  NODE *last;
+int main() {
+    NODE head, second, last;
 
-  head = NewNode(1);
-  second = NewNode(2);
-  last = NewNode(3);
+    head = NewNode(1);
+    second = NewNode(2);
+    last = NewNode(3);
 
-  head->link = second;
-  second->link = last;
+    head->link = second; // Link first node to second
+    second->link = last; // Link second node to last
 
-  printf("Linked List: \n");
-  DisplayNode(head);
-  return 0;
+    printf("Linked List:\n");
+    DisplayNode(head); // Display the linked list
+
+    // Free allocated memory
+    free(last);
+    free(second);
+    free(head);
+
+    return 0;
 }
